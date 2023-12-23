537af69da966a7c7ecaf59a84e74e5004d185e4f
rensawamo <106803080+rensawamo@users.noreply.github.com>
2023-12-23 09:59:50 +0900

    Exercise: Readers

---
+++ Readers/main.go
@@ -0,0 +1,54 @@
+// Exercise: Readers
+// ASCII文字 'A' の無限ストリームを出力する Reader 型を実装してください。
+
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/reader"
+)
+
+type MyReader struct{}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (r MyReader) Read(b []byte) (int, error) {
+	if b == nil {
+		return 0, fmt.Errorf("nil error")
+	}
+	for i := range b {
+		b[i] = 'A'
+	}
+	fmt.Print("実行") // 無限ストリームの証明
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
+
+//テスト構文
+//  Validateは   1mbにforか lenが達したときにとじられて okが出力
+// func Validate(r io.Reader)
+// 	b := make([]byte, 1024, 2048)
+// 	i, o := 0, 0
+// 	for ; i < 1<<20 && o < 1<<20; i++ { // test 1mb
+// 		n, err := r.Read(b)
+// 		for i, v := range b[:n] {
+// 			if v != 'A' {
+// 				fmt.Fprintf(os.Stderr, "got byte %x at offset %v, want 'A'\n", v, o+i)
+// 				return
+// 			}
+// 		}
+// 		o += n
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "read error: %v\n", err)
+// 			return
+// 		}
+// 	}
+// 	if o == 0 {
+// 		fmt.Fprintf(os.Stderr, "read zero bytes after %d Read calls\n", i)
+// 		return
+// 	}
+// 	fmt.Println("OK!")
+// }
